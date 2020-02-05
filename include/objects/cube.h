2ea27f3808b3113749a4eae03deb5c031380dc69
CoenB95 <boelhouwers.coen@gmail.com>
2020-02-05 15:16:32 +0100

    Fix cube never returned.

--- include/objects/cube.h
+++ include/objects/cube.h
@@ -1,6 +1,6 @@
 #include "components/primitivebuildcomponent.h"
 #include "components/spincomponent.h"
-#include "components/texturedrawcomponent.h"
+#include "components/primitivedrawcomponent.h"
 #include "objects/gameobject.h"
 
 namespace gamo {
@@ -13,13 +13,15 @@ namespace gamo {
           Cube* cube = new Cube();
           cube->addComponent(new SpinComponent(10.0));
           cube->addComponent(new ColoredCubeBuildComponent(size));
-          cube->addComponent(new TextureDrawComponent("res/terrain.png"));
+          cube->addComponent(new ColorDrawComponent());
+          return cube;
       };
       static Cube* textured(double size = 1) {
           Cube* cube = new Cube();
           cube->addComponent(new SpinComponent(10.0));
           cube->addComponent(new TexturedCubeBuildComponent(size));
           cube->addComponent(new TextureDrawComponent("res/terrain.png"));
+          return cube;
       };
   };
 }
